0d3fa7accedeeec4beddfc6ada12f09a55b9eed3
Anna <k24i3@mail.ru>
2022-07-12 10:16:10 +0400

    ex03

---
+++ 03/Program.cs
@@ -0,0 +1,16 @@
+﻿//с клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+double a,b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+if (a>b)
+{ 
+    System.Console.WriteLine($"{a} max");
+}
+else
+{
+    System.Console.WriteLine($"{b} max");
+}
