850360feff70e1064b5889a63bc1c3f20982f977
Anna <k24i3@mail.ru>
2022-07-12 10:27:34 +0400

    ex12

---
+++ 12/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+int a,b,c;
+string? s;
+s=Console.ReadLine();//просит ввести число
+a=Convert.ToInt32(s);//преобразует формат числа в int
+b=a/10;
+c=a%10;
+
+if (c>b)
+{ 
+    System.Console.WriteLine(c);
+}
+else
+{
+    System.Console.WriteLine(b);
+}
