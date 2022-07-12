c8c6a08650c29363835dfb6ab8c349cb257916e5
Anna <k24i3@mail.ru>
2022-07-12 11:26:47 +0400

    ex15

---
+++ 15/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+
+int a,b,c;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a%1000;
+c=b/100;
+
+if (c>0)
+{
+    System.Console.WriteLine(c);
+}
+else
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
