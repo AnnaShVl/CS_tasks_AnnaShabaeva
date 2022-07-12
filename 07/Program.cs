51ad15a3b46971445ea6af1c62eaef95f1cc0b66
Anna <k24i3@mail.ru>
2022-07-12 10:20:41 +0400

    ex07

---
+++ 07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+int a;
+string? s;
+s=Console.ReadLine();//просит ввести число
+a=Convert.ToInt32(s);
+if (a%2==0)
+{
+     System.Console.WriteLine($"{a} четное");
+}
+else
+{
+     System.Console.WriteLine($"{a} нечетное");
+}
