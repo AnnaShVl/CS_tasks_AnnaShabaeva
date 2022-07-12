3b9d30bc9348985c53b8e8763ce9e0ae1cd2779d
Anna <k24i3@mail.ru>
2022-07-12 10:24:04 +0400

    ex09

---
+++ 09/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вывести на экран четные числа от 1 до N
+
+int i=0;
+int N;
+string? s;
+s=Console.ReadLine();//просит ввести число
+N=Convert.ToInt32(s);//преобразует формат числа в int
+while (i<=N)
+{
+    System.Console.Write($"{i}  ");
+    i=i+2;
+}
