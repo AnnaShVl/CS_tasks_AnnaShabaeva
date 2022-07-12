851a7900d37021736efd98ea21001b03161b6ea3
Anna <k24i3@mail.ru>
2022-07-12 10:18:25 +0400

    ex06

---
+++ 06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+
+double a,b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+b=Math.Sin(a);
+System.Console.WriteLine($"{b}");
