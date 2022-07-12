9244614d76b99bea2870455958bdabcb536298e5
Anna <k24i3@mail.ru>
2022-07-12 10:22:20 +0400

    ex08

---
+++ 08/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран числа от -N до N
+
+int N;
+string? s;
+s=Console.ReadLine();//просит ввести число
+N=Convert.ToInt32(s);//преобразует формат числа в int
+
+int i=-N;
+while (i<=N)
+{
+    System.Console.Write($"{i}  ");
+    i=i+1;}
