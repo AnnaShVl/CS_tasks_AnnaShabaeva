9c83aaf7c009715750a7ea8d7f336d1c76edb4d4
Anna <k24i3@mail.ru>
2022-07-12 11:01:00 +0400

    ex11

---
+++ 11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+
+int a,b,c;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a/10;
+c=b%10;
+
+System.Console.WriteLine(c);
