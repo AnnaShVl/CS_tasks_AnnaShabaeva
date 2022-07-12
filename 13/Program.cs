3def5975a20ea24dd6fad83dbede5b3543bbef5e
Anna <k24i3@mail.ru>
2022-07-12 11:08:03 +0400

    ex13

---
+++ 13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+
+int a,b,c;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a%10;
+c=a/100;
+
+System.Console.WriteLine($"{c}{b}");
