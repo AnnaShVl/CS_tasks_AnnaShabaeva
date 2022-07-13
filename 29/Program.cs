8d7367ae33dc6dbb2b9bcca018836a8faca5d0d1
Anna <k24i3@mail.ru>
2022-07-13 10:26:14 +0400

    Ex29

---
+++ 29/Program.cs
@@ -0,0 +1,19 @@
+﻿// 29. Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите целое число");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int sum, div, a;
+sum=0;
+div=1;
+a=1;
+
+while (a!=0)
+{
+    a=(num%(div*10))/div;
+    sum=sum+a;
+    div=div*10;
+}
+ Console.WriteLine(sum);
+
+
