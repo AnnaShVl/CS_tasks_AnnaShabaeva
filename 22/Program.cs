28e1f4df27490bc59a4b0fb12c745ee03c61f372
Anna <k24i3@mail.ru>
2022-07-13 09:56:28 +0400

    Ex22

---
+++ 22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int a,b,c,d;
+a=num%10;
+b=(num%100)/10;
+c=(num%10000)/1000;
+d=num/10000;
+
+if (a==d && b==c)
+{
+    Console.WriteLine("Число - палиндром");
+}
+else
+{
+    Console.WriteLine("Число - не палиндром");
+}
+
+//Console.WriteLine($"{a} {b} {c} {d}");
