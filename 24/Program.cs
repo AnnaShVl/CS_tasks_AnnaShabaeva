8f20ae2af920ac5306de3e8e09ad10ac28e6a402
Anna <k24i3@mail.ru>
2022-07-14 12:09:30 +0400

    Ex24

---
+++ 24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+int a, b;
+a=1;
+b=1;
+
+while (a<=N)
+{
+    b=a*a;
+    a++;
+     Console.Write($"{b} ");
+}
+
+
