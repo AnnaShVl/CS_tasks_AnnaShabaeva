7d4a2523ecb1f031db1010f999897da0497ab379
Anna <k24i3@mail.ru>
2022-07-14 12:12:00 +0400

    Ex25

---
+++ 25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+int a, b;
+a=1;
+b=1;
+
+while (a<=N)
+{
+    b=a*a*a;
+    a++;
+     Console.Write($"{b} ");
+}
