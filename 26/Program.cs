30ae21da8d5403dffab0ce5ddff3912371a5f4e3
Anna <k24i3@mail.ru>
2022-07-14 12:15:43 +0400

    Ex26

---
+++ 26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите целое число");
+int A=Convert.ToInt32 (Console.ReadLine());
+
+int n, s;
+n=1;
+s=0;
+
+while (n<=A)
+{
+    s=s+n;
+    n++;
+}
+
+Console.Write(s);
