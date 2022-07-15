dd89e93c65be602fbacd40b66941788988233b9e
Anna <k24i3@mail.ru>
2022-07-15 11:48:54 +0400

    Ex28

---
+++ 28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+
+Console.WriteLine("Введите целое число");
+int A=Convert.ToInt32 (Console.ReadLine());
+
+System.Console.WriteLine(CountDigits(A));
+
+int CountDigits(int N)//8-18 строки-подпрограмма
+{
+    if (N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N/=10;        
+    }
+    return k;
+}
