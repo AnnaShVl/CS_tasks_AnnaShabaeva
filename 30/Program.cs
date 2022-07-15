9f462714eb058b444020290369b0bf5ea88b014b
Anna <k24i3@mail.ru>
2022-07-15 12:04:33 +0400

    Ex30

---
+++ 30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+System.Console.WriteLine(Factorial(N));
+
+int Factorial(int N)//8-17 строки-подпрограмма
+{
+    int f=1, a=1;
+    while(a<N)
+    {
+        a++;
+        f=f*a;        
+    }
+    return f;
+}
