fe90512328dae610bd6a3915b116fef58fbee436
Anna <k24i3@mail.ru>
2022-07-15 12:09:51 +0400

    Ex31

---
+++ 31/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+int a, b;
+a=1;
+b=1;
+
+while (a<=N)
+{
+    b=a*a*a;
+    a++;
+    if (b%2==0)
+    {
+        Console.Write($"{b} ");
+    }  
+}
