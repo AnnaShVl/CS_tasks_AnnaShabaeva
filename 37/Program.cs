b5c64d456e842a266b721b6a338efc5b9f83b08a
Anna <k24i3@mail.ru>
2022-07-17 11:15:38 +0400

    Ex37

---
+++ 37/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+
+int N=10;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(100,1000);
+}
+int countEven=0;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]%2==0)
+    {
+        countEven=countEven+1;
+    }
+}
+
+int countOdd=0;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]%2!=0)
+    {
+        countOdd+=1;
+    }
+}
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+System.Console.WriteLine();
+System.Console.WriteLine($"количество четных:{countEven}");
+System.Console.WriteLine($"количество нечетных:{countOdd}");
