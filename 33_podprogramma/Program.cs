1ce995dbf986915bda284fcf1a1c552a88d73e49
Anna <k24i3@mail.ru>
2022-08-08 16:14:09 +0400

    added 33_podprogramma

---
+++ 33_podprogramma/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int N=8;
+int[] a=new int[N];
+
+
+//Ввод данных
+Init(a,N);
+
+Print(a);
+
+//инициализация данных
+void Init(int[] a,int Length)
+{
+    Random random=new Random();//псевдослучайные числа
+    for(int i=0;i<a.Length;i++)
+            a[i]=random.Next(0,2);
+}
+
+
+//Вывод результатов
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],4}");
+}
+
+
