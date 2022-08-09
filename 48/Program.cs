38781783eaef9d6a3684d9e68ca7aecd28ed57fe
Anna <k24i3@mail.ru>
2022-08-09 16:12:45 +0400

    added 48

---
+++ 48/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Написать программу копирования массива
+
+int N=8;
+int[] arra=new int[N];
+Init(arra,N);
+Print(arra);
+int[] arrb=new int[N];
+CopyArray(arrb);
+Print(arrb);
+
+void CopyArray(int[] arrb)
+{
+     for(int i=0;i<arrb.Length;i++)
+            arrb[i]=arra[i];
+            
+}
+
+void Init(int[] a,int N)
+{
+    Random random=new Random();
+    for(int i=0;i<a.Length;i++)
+            a[i]=random.Next(0,11);
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],4}");
+        System.Console.WriteLine();
+}
+
