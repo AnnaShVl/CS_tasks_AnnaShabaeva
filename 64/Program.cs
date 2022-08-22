f01ff055ccfd9e475ecf3a7be5a118c8f3a6608a
Anna <k24i3@mail.ru>
2022-08-22 14:46:45 +0400

    added 64

---
+++ 64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите целое число");
+int M=Convert.ToInt32 (Console.ReadLine());
+
+
+if (M<N)
+{
+    while (M<=N)
+    {
+        Console.Write($"{M} ");
+        M++;
+   
+    }
+}
+else
+{
+    while (N<=M)
+    {
+        Console.Write($"{N} ");
+        N++;
+   
+    }
+}
