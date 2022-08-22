cdff2d877aab7d28c5b0e07b0ebcbf31a082582c
Anna <k24i3@mail.ru>
2022-08-22 14:46:55 +0400

    added 66

---
+++ 66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите целое число");
+int M=Convert.ToInt32 (Console.ReadLine());
+
+int sum=0;
+
+if (M<N)
+{
+    while (M<=N)
+    {
+        sum=sum+M;
+        M++; 
+    }
+    Console.WriteLine(sum);
+}
+else
+{
+    while (N<=M)
+    {
+        sum=sum+N;
+        N++;
+    }
+    Console.WriteLine(sum);
+}
